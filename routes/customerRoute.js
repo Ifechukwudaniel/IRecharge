398e18d17c5bc855d6131271465b896e97bdd3ed
Angry Coding <mailidanielifechukwu@gmail.com>
2022-08-23 18:10:57 +0100

    payment and  charge otp

--- routes/customerRoute.js
+++ routes/customerRoute.js
@@ -4,5 +4,7 @@ const customerCtrl = require("../controllers/customer");
 Router.route("/").post(customerCtrl.create);
 Router.route("/").get(customerCtrl.getAllCustomers);
 Router.route("/charge/:id/").post(customerCtrl.chargeCustomer);
+Router.route("/chargeOtp/:id/").post(customerCtrl.validateCharge);
 Router.route("/:id").get(customerCtrl.getCustomerById);
+Router.route("/payments/:id").get(customerCtrl.getCustomerPurchases);
 module.exports = Router;
