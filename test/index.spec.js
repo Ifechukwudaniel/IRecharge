bf769e9dc621d7239ed2ef2418fb76aa7773a7e9
Angry Coding <mailidanielifechukwu@gmail.com>
2022-08-24 10:45:27 +0100

    get customer test

--- test/index.spec.js
+++ test/index.spec.js
@@ -13,22 +13,9 @@ describe("GET /", function () {
       });
   });
 
-  it("it should has status code 200", function (done) {
+  it("it should get customers status code 200", function (done) {
     supertest(app)
       .get("/api/customers/")
-      .expect(200)
-      .end(function (err, res) {
-        if (err) done(err);
-        done();
-      });
-  });
-
-  it("it should has status code 200", function (done) {
-    supertest(app)
-      .post("/api/customers/", {
-        name: "Ifechuku  Daniel",
-        email: "dandynamicx@gmail.comsjjjj",
-      })
       .expect(200)
       .end(function (err, res) {
         if (err) done(err);
